6129ade68e367570521ca93a979a20ea4d6c0b16
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-10-04 23:42:46 -0500

    Started building out the boxes and added very basic functionality so I can start planning

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
-console.log('All linked Up')
+const player = {
+    playerTurn: false
+}
+
+
+
+let boxes = document.querySelectorAll(".tiles")
+
+
+
+boxes.forEach((box) => {
+    box.addEventListener('click', function choice() {
+        if(player.playerTurn) {
+            box.innerHTML = "X"
+        }
+    })
+})
+
