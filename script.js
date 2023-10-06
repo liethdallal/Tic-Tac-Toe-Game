dccab4e24001f355a80d5a870b1964898f7f6dfa
liethdallal <139842069+liethdallal@users.noreply.github.com>
2023-10-05 20:14:40 -0500

    Added some basic functionality

--- script.js
+++ script.js
@@ -1,18 +1,50 @@
-const player = {
+const player1 = {
+    playerTurn: true
+}
+
+const player2 = {
     playerTurn: false
 }
 
 
 
-let boxes = document.querySelectorAll(".tiles")
+let boxes = document.querySelectorAll('.tiles')
+let player1Message= document.querySelector('.player1message')
+let player2Message= document.querySelector('.player2message')
 
 
+function winConditions() {
+    return
+}
 
 boxes.forEach((box) => {
-    box.addEventListener('click', function choice() {
-        if(player.playerTurn) {
-            box.innerHTML = "X"
+    box.addEventListener('click', () => {
+        if (box.classList.contains('selected')) {
+            return; // Exit the function if the box is already selected
+        } else {
+            // Add the selected class to mark the box as selected
+            box.classList.add('selected');
+
+            if (player1.playerTurn) {
+                box.innerHTML = "X";
+                player1Message.style.display = 'none'
+                player2Message.style.display = 'block'
+                setTimeout(() => {
+                    player2.playerTurn = true;
+                    player1.playerTurn = false;
+                   
+
+                });
+            } else if (player2.playerTurn) {
+                box.innerHTML = "O";
+                player1Message.style.display = 'block'
+                player2Message.style.display = 'none'
+                setTimeout(() => {
+                    player2.playerTurn = false;
+                    player1.playerTurn = true;
+                });
+            }
         }
-    })
-})
+    });
+});
 
